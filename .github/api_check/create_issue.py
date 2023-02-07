41adb71505cea8b3ae881fd463ca6f4e4b7f4497
Maxim Mosin <max@mosin.pw>
2023-02-08 05:43:23 +0900

    Add diff of new bundle to issue

--- .github/api_check/create_issue.py
+++ .github/api_check/create_issue.py
@@ -4,8 +4,11 @@ import json
 import argparse
 import subprocess as sp
 
+import httpx
+
 
 ISSUE_PATTERN = re.compile(r'\[API\] New version: (?P<etag>.+)')
+OPENAPI_URL = 'https://timeweb.cloud/api-docs-data/bundle.json'
 argparser = argparse.ArgumentParser(description='Create issue about new API version.')
 argparser.add_argument('etag', help='New API version etag.')
 argparser.add_argument('--get-link', action='store_true', help='Print issue link and exit.', default=False)
@@ -14,7 +17,7 @@ args = argparser.parse_args()
 
 def have_active_issue(etag: str) -> bool:
     list_issue = sp.check_output(
-        ['gh', 'issue', 'list', '--json', 'title', '--label', 'tw-api', '--state', 'open']
+        ['gh', 'issue', 'list', '--json', 'title', '--label', 'timeweb.cloud', '--state', 'open']
     )
     list_issue_data: list[dict] = json.loads(list_issue.decode('utf-8'))
     for issue in list_issue_data:
@@ -26,7 +29,8 @@ def have_active_issue(etag: str) -> bool:
 
 def get_issue_link(etag: str) -> str:
     list_issue = sp.check_output(
-        ['gh', 'issue', 'list', '--json', 'title,url', '--label', 'tw-api', '--state', 'open']
+        ['gh', 'issue', 'list', '--json', 'title,url',
+            '--label', 'timeweb.cloud', '--state', 'open']
     )
     list_issue_data: list[dict] = json.loads(list_issue.decode('utf-8'))
     for issue in list_issue_data:
@@ -36,11 +40,26 @@ def get_issue_link(etag: str) -> str:
     return ''
 
 
+def get_issue_body() -> str:
+    resp = httpx.get(OPENAPI_URL)
+    resp.raise_for_status()
+    with open('bundle.json', 'wb') as f:
+        f.write(resp.content)
+    diff = sp.check_output(['git', 'diff', '--no-index', '.github/api_check/bundle.json', 'bundle.json'])
+    diff_data = diff.decode('utf-8')
+    return f'''New API version is available.
+
+    ```diff
+    {diff_data}
+    ```
+    '''
+
+
 def create_issue(etag: str) -> None:
     params = [
         'gh', 'issue', 'create', '--title',
-        f'[API] New version: {etag}', '--label', 'tw-api',
-        '--body', 'New API version is available.'
+        f'[API] New version: {etag}', '--label', 'timeweb.cloud',
+        '--body', get_issue_body()
     ]
     sp.run(params)
 
