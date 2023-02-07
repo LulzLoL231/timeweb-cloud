268de2bf991a1d8d8a928b0e00ea882a6cc95f0e
Maxim Mosin <max@mosin.pw>
2023-02-08 04:26:34 +0900

    Add issue url to tg msg

--- .github/api_check/create_issue.py
+++ .github/api_check/create_issue.py
@@ -8,6 +8,7 @@ import subprocess as sp
 ISSUE_PATTERN = re.compile(r'\[API\] New version: (?P<etag>.+)')
 argparser = argparse.ArgumentParser(description='Create issue about new API version.')
 argparser.add_argument('etag', help='New API version etag.')
+argparser.add_argument('--get-link', action='store_true', help='Print issue link and exit.', default=False)
 args = argparser.parse_args()
 
 
@@ -23,6 +24,18 @@ def have_active_issue(etag: str) -> bool:
     return False
 
 
+def get_issue_link(etag: str) -> str:
+    list_issue = sp.check_output(
+        ['gh', 'issue', 'list', '--json', 'title,url', '--label', 'tw-api', '--state', 'open']
+    )
+    list_issue_data: list[dict] = json.loads(list_issue.decode('utf-8'))
+    for issue in list_issue_data:
+        match = ISSUE_PATTERN.match(issue['title'])
+        if match and match.group('etag') == etag:
+            return issue['url']
+    return ''
+
+
 def create_issue(etag: str) -> None:
     params = [
         'gh', 'issue', 'create', '--title',
@@ -32,8 +45,12 @@ def create_issue(etag: str) -> None:
     sp.run(params)
 
 
-if not have_active_issue(args.etag):
-    create_issue(args.etag)
+if args.get_link:
+    print(get_issue_link(args.etag))
     exit(0)
-print('Issue already exists.')
-exit(1)
+else:
+    if not have_active_issue(args.etag):
+        create_issue(args.etag)
+        exit(0)
+    print('Issue already exists.')
+    exit(1)
