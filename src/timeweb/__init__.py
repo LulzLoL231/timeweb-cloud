62e17dffca6436067dede261ecb9496aa392827f
Maxim Mosin <max@mosin.pw>
2023-02-09 00:45:09 +0900

    Add AsyncTimeweb to module exports

--- src/timeweb/__init__.py
+++ src/timeweb/__init__.py
@@ -3,10 +3,12 @@
 '''Timeweb Cloud API client'''
 from .__meta import __version__, __author__
 from .sync_api.api import Timeweb
+from .async_api.api import AsyncTimeweb
 
 
 __all__ = [
     'Timeweb',
+    'AsyncTimeweb',
     '__version__',
     '__author__',
 ]
